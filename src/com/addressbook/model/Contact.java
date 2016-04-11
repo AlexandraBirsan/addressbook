a1c52f67ff99dc7a3780ec5f920dc7355a309693
birsan <alexandra.birsan95@yahoo.com>
2016-04-11 15:12:08 +0300

    Added model classes

--- src/com/addressbook/model/Contact.java
+++ src/com/addressbook/model/Contact.java
@@ -1,7 +1,60 @@
 package com.addressbook.model;
 
+import java.util.List;
+import java.util.Vector;
+
 /**
  * Created by birsan on 4/11/2016.
  */
 public class Contact {
+    private String firstName;
+    private String lastName;
+    private String company;
+    private byte[] photo;
+    private List<PhoneNumber> phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Contact setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Contact setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public Contact setCompany(String company) {
+        this.company = company;
+        return this;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public Contact setPhoto(byte[] photo) {
+        this.photo = photo;
+        return this;
+    }
+
+    public List<PhoneNumber> getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Contact setPhoneNumber(List<PhoneNumber> phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
 }
---
+++ src/com/addressbook/model/PhoneNumber.java
@@ -0,0 +1,27 @@
+package com.addressbook.model;
+
+/**
+ * Created by birsan on 4/11/2016.
+ */
+public class PhoneNumber {
+    private long contactId;
+    private String number;
+
+    public long getContactId() {
+        return contactId;
+    }
+
+    public PhoneNumber setContactId(long contactId) {
+        this.contactId = contactId;
+        return this;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public PhoneNumber setNumber(String number) {
+        this.number = number;
+        return this;
+    }
+}
